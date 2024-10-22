06f729696a3eab8531cacfa4689a229df6e72f1c
arch-fan <juanserracines@gmail.com>
2024-10-22 16:34:45 +0200

    Hyprland

--- hosts/dam/configuration.nix
+++ hosts/dam/configuration.nix
@@ -9,6 +9,7 @@
 }: {
   imports = [
     ./hardware-configuration.nix
+    ./display/hyprland.nix
     home-manager.nixosModules.default
   ];
 
@@ -60,10 +61,6 @@
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
---
+++ hosts/dam/display/hyprland.nix
@@ -0,0 +1,7 @@
+{...}: {
+  # TEMP
+  services.xserver.displayManager.gdm.enable = true;
+  # ENDTEMP
+
+  programs.hyprland.enable = true;
+}
